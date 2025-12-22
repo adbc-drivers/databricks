175bd70850bba138fada3f911e5190db962e1c3f
github-action-benchmark <github@users.noreply.github.com>
2025-12-22 00:23:07 +0000

    add Peak Memory (.NET 8.0) (customSmallerIsBetter) benchmark result for e6224770def90297e4f7d375b646a4761aa0e4a5

--- bench/peak-memory/net8/data.js
+++ bench/peak-memory/net8/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765958334728,
+  "lastUpdate": 1766362986886,
   "repoUrl": "https://github.com/adbc-drivers/databricks",
   "entries": {
     "Peak Memory (.NET 8.0)": [
@@ -766,6 +766,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "wide_sales_analysis",
             "value": 479.34,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "petridish@gmail.com",
+            "name": "Bryce Mecum",
+            "username": "amoeba"
+          },
+          "committer": {
+            "email": "petridish@gmail.com",
+            "name": "Bryce Mecum",
+            "username": "amoeba"
+          },
+          "distinct": true,
+          "id": "e6224770def90297e4f7d375b646a4761aa0e4a5",
+          "message": "chore(go): migrate go driver from ADBC Repo\n\nMigrates the Go Databricks driver from the ADBC repo to the `./go` directory in this repo. This PR includes changes to port the driver to the new driverbase and make it build and test cleanly. There was only one commit to migrate and I did that by running git filter-repo with `--path-rename`:\n\n```sh\ngit filter-repo --force --path go/adbc/driver/databricks --path go/adbc/pkg/databricks/ --path-rename go/adbc/driver/databricks:go --path-rename go/adbc/pkg/databricks:go/pkg\n```\n\nand then cherry-picking the commit on this brach. This gives us a new hash but preserves the original commit including date.\n\nThis PR also makes a number of changes to the existing C# driver, mostly to CI files, to make this all work together better.\n\nCloses #31",
+          "timestamp": "2025-12-21T16:10:09-08:00",
+          "tree_id": "96f7d4aacf938dd537a3204a97315ff54bbe6c16",
+          "url": "https://github.com/adbc-drivers/databricks/commit/e6224770def90297e4f7d375b646a4761aa0e4a5"
+        },
+        "date": 1766362986136,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "catalog_sales",
+            "value": 533.46,
+            "unit": "MB"
+          },
+          {
+            "name": "customer",
+            "value": 361.07,
+            "unit": "MB"
+          },
+          {
+            "name": "inventory",
+            "value": 497.07,
+            "unit": "MB"
+          },
+          {
+            "name": "sales(...)tamps_[21]",
+            "value": 471.53,
+            "unit": "MB"
+          },
+          {
+            "name": "store_sales_numeric",
+            "value": 458.91,
+            "unit": "MB"
+          },
+          {
+            "name": "web_sales",
+            "value": 479.62,
+            "unit": "MB"
+          },
+          {
+            "name": "wide_sales_analysis",
+            "value": 511.12,
             "unit": "MB"
           }
         ]
