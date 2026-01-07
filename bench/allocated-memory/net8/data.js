98ef3408537ae1c04dead3db2cbe086d2af1b934
github-action-benchmark <github@users.noreply.github.com>
2026-01-07 00:18:45 +0000

    add Allocated Memory (.NET 8.0) (customSmallerIsBetter) benchmark result for 0bff54c1e37649d81d61cd442a3fbbaa73714078

--- bench/allocated-memory/net8/data.js
+++ bench/allocated-memory/net8/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767725444409,
+  "lastUpdate": 1767745125370,
   "repoUrl": "https://github.com/adbc-drivers/databricks",
   "entries": {
     "Allocated Memory (.NET 8.0)": [
@@ -884,6 +884,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "wide_sales_analysis",
             "value": 1169.89,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "13318837+davidhcoe@users.noreply.github.com",
+            "name": "davidhcoe",
+            "username": "davidhcoe"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0bff54c1e37649d81d61cd442a3fbbaa73714078",
+          "message": "feat(csharp): align project structures and output locations to other drivers (#115)\n\n## What's Changed\n\n- aligns the project structure and output location (artifacts) with the\nother ADBC drivers\n- added Directory.Build.props file\n\nCo-authored-by: David Coe <>",
+          "timestamp": "2026-01-06T19:05:54-05:00",
+          "tree_id": "f396118726ef8d5e2d266b58b5d9e7c9ea0f0332",
+          "url": "https://github.com/adbc-drivers/databricks/commit/0bff54c1e37649d81d61cd442a3fbbaa73714078"
+        },
+        "date": 1767745124731,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "catalog_sales",
+            "value": 276.85,
+            "unit": "MB"
+          },
+          {
+            "name": "customer",
+            "value": 37.11,
+            "unit": "MB"
+          },
+          {
+            "name": "inventory",
+            "value": 206.28,
+            "unit": "MB"
+          },
+          {
+            "name": "sales(...)tamps_[21]",
+            "value": 105.3,
+            "unit": "MB"
+          },
+          {
+            "name": "store_sales_numeric",
+            "value": 343.79,
+            "unit": "MB"
+          },
+          {
+            "name": "web_sales",
+            "value": 151.28,
+            "unit": "MB"
+          },
+          {
+            "name": "wide_sales_analysis",
+            "value": 1142.57,
             "unit": "MB"
           }
         ]
