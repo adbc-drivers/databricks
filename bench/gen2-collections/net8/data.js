b44133b559b7e81122e4ea9888ae26910802e686
github-action-benchmark <github@users.noreply.github.com>
2026-01-07 00:18:47 +0000

    add Gen2 Collections (.NET 8.0) (customSmallerIsBetter) benchmark result for 0bff54c1e37649d81d61cd442a3fbbaa73714078

--- bench/gen2-collections/net8/data.js
+++ bench/gen2-collections/net8/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767725445439,
+  "lastUpdate": 1767745127126,
   "repoUrl": "https://github.com/adbc-drivers/databricks",
   "entries": {
     "Gen2 Collections (.NET 8.0)": [
@@ -884,6 +884,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "wide_sales_analysis",
             "value": 71,
+            "unit": "collections"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "13318837+davidhcoe@users.noreply.github.com",
+            "name": "davidhcoe",
+            "username": "davidhcoe"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0bff54c1e37649d81d61cd442a3fbbaa73714078",
+          "message": "feat(csharp): align project structures and output locations to other drivers (#115)\n\n## What's Changed\n\n- aligns the project structure and output location (artifacts) with the\nother ADBC drivers\n- added Directory.Build.props file\n\nCo-authored-by: David Coe <>",
+          "timestamp": "2026-01-06T19:05:54-05:00",
+          "tree_id": "f396118726ef8d5e2d266b58b5d9e7c9ea0f0332",
+          "url": "https://github.com/adbc-drivers/databricks/commit/0bff54c1e37649d81d61cd442a3fbbaa73714078"
+        },
+        "date": 1767745126444,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "catalog_sales",
+            "value": 65,
+            "unit": "collections"
+          },
+          {
+            "name": "customer",
+            "value": 2,
+            "unit": "collections"
+          },
+          {
+            "name": "inventory",
+            "value": 23,
+            "unit": "collections"
+          },
+          {
+            "name": "sales(...)tamps_[21]",
+            "value": 41,
+            "unit": "collections"
+          },
+          {
+            "name": "store_sales_numeric",
+            "value": 58,
+            "unit": "collections"
+          },
+          {
+            "name": "web_sales",
+            "value": 45,
+            "unit": "collections"
+          },
+          {
+            "name": "wide_sales_analysis",
+            "value": 107,
             "unit": "collections"
           }
         ]
