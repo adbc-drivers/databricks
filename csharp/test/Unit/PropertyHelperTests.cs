2cc735699d355b60016dcaae4c88e74bbcfe351e
Jade Wang <jade.wang+data@databricks.com>
2026-03-19 01:13:58 +0000

    GetStringProperty - Returns value when key exists\n\nTask: 1.1.1 (Phase 1)

---
+++ csharp/test/Unit/PropertyHelperTests.cs
@@ -0,0 +1,48 @@
+/*
+* Copyright (c) 2025 ADBC Drivers Contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdbcDrivers.Databricks.Tests
+{
+    /// <summary>
+    /// Unit tests for the PropertyHelper class.
+    /// </summary>
+    public class PropertyHelperTests
+    {
+        /// <summary>
+        /// Test that GetStringProperty returns the correct value when the key exists.
+        /// </summary>
+        [Fact]
+        public void GetStringProperty_ReturnsValueWhenKeyExists()
+        {
+            // Arrange
+            var properties = new Dictionary<string, string>
+            {
+                { "key1", "value1" },
+                { "key2", "value2" }
+            };
+
+            // Act
+            var result = PropertyHelper.GetStringProperty(properties, "key1", "default");
+
+            // Assert
+            Assert.Equal("value1", result);
+        }
+    }
+}
